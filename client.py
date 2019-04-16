9d28a06a15147222df42e9bbb2ad70afdc201146
Kajetan <drakles1@gmail.com>
2019-04-16 20:52:24 +0200

    now really short and ugly AF

--- client.py
+++ client.py
@@ -1,7 +1,2 @@
 #!/usr/bin/env python3
-import socket as s
-z = s.socket(s.AF_INET, s.SOCK_STREAM)
-z.connect(('',1))
-z.send(b'!')
-d = z.recv(8)
-print(d)
+import socket as s;z=s.socket(s.AF_INET,s.SOCK_STREAM);z.connect(('',1));z.send(b'!');d=z.recv(8);print(d)
